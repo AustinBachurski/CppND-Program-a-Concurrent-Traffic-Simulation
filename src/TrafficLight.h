27f130641516cda66504f945a9401998b2673435
Austin Bachurski <austinbachurski@gmail.com>
2023-04-10 10:02:27 -0600

    first attempt

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -4,50 +4,56 @@
 #include <mutex>
 #include <deque>
 #include <condition_variable>
+#include <random>
 #include "TrafficObject.h"
 
 // forward declarations to avoid include cycle
 class Vehicle;
 
+enum class TrafficLightPhase
+{
+    red,
+    green,
+}
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
-// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
-// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
+/******************************************************************************
+ *  Message Queue Class */
 
 template <class T>
 class MessageQueue
 {
 public:
+    TrafficLightPhase receive();
+    void send(T&& phase);
 
 private:
-    
+    std::condition_variable _condition;
+    std::deque<T> _queue;
+    std::mutex _mutex;
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+/******************************************************************************
+ *  Traffic Light Class */
+
+
+class TrafficLight : public TrafficObject
 {
 public:
-    // constructor / desctructor
+    TrafficLight();
 
-    // getters / setters
+    void waitForGreen();
+    void simulate() override;
+    TrafficLightPhase getCurrentPhase();
 
-    // typical behaviour methods
 
 private:
-    // typical behaviour methods
-
-    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
-    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
-    // send in conjunction with move semantics.
-
     std::condition_variable _condition;
+    TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _messageQueue;
     std::mutex _mutex;
+    void cycleThroughPhases();
+    int GetRandomNumberInRange(int lowerBound, int upperBound);
 };
 
 #endif
